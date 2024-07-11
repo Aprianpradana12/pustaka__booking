3c7468df773d416a51ce7d05deb0581d1f1503bb
Aprianpradana12 <javadillah@gmail.com>
2024-07-11 15:29:38 +0700

    sudah menyelesaikan pustaka booking

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Adi</li>
+		<li>Nama Belakang : Pangestu</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jaticempaka-Kota Bekasi</li>
+	</ul>
+	
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Megang Sakti-sumatera Selatan</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li></li>
+	</ul>
+	
+
+</section>
